0a2eacbfda22fc980ddeb659dc5693e6e735b603
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-02-23 04:40:27 +0000

    fix: remove old schema code

--- src/types.js
+++
@@ -1,7 +0,0 @@
-const gen = require('../../js-schema-gen') // require('@ipld/schema-gen')
-const schema = require('./schema.json')
-
-module.exports = (Block, codec) => {
-  const types = gen(schema)
-  return types
-}
