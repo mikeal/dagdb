cdeaf566d5c72d355c9e1f5ffac635ddd743a680
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-02-26 04:23:25 +0000

    fix: make the linter happy

--- src/store/inmemory.js
+++ src/store/inmemory.js
@@ -2,10 +2,12 @@ class InMemory {
   constructor () {
     this.storage = new Map()
   }
+
   async put (block) {
     const cid = await block.cid()
     this.storage.set(cid.toString('base64'), block)
   }
+
   async get (cid) {
     const key = cid.toString('base64')
     const value = this.storage.get(key)
