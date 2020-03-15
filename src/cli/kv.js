ff3fdedf60815f3a5769fe280851e664f1301ed8
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-03-15 18:59:23 +0000

    wip: moving a few things around

--- src/cli/kv.js
+++
@@ -1,4 +0,0 @@
-exports.command = 'kv <command>'
-exports.desc = 'Commands for interacting with key-value stores'
-exports.builder = yargs => yargs.commandDir('kv_cmds')
-exports.handler = argv => {}
